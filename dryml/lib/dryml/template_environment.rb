942875802877baa82e17f68c35ba06aac06a5859
Ignacio Huerta <ignacio@ihuerta.net>
2014-12-23 23:15:38 +0100

    Fix unit test: Remove ugly Rails 4 hack with nicer loading of sprockets

--- dryml/lib/dryml/template_environment.rb
+++ dryml/lib/dryml/template_environment.rb
@@ -44,11 +44,7 @@ module Dryml
             instance_variable_set(iv, view.instance_variable_get(iv))
           end
 
-          # Rails 4 hack.  Our method_missing hack doesn't get these because they're already (imporperly) defined.  Not sure why
-          self.assets_prefix = view.assets_prefix
-          self.assets_environment = view.assets_environment
-          self.assets_manifest = view.assets_manifest
-          self.digest_assets = view.digest_assets
+          view.extend ::Sprockets::Rails::Helper
         end
       end
     end
